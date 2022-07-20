2f9edf45828a069d0ae39c7efb8f9c6eb5d2b58d
shagun <shagun08081999@gmail.com>
2022-07-20 22:04:40 +0530

    day one task compleat

---
+++ components/navbar.js
@@ -0,0 +1,99 @@
+let navbar=()=>{
+    return `<div id="left">
+    <div><img src="https://www.hotjar.com/images/hotjar-logo-small.svg" alt="">
+    <h1><a href="index.html">hotjar</a></h1>
+    </div>
+     <div id="product">Product </div>
+     <div id="whyhotraj">Why Hotraj</div>
+     <div id="pricing">Pricing</div>
+     <div id="learning">Learning</div>
+     <div id="company">Company</div>
+ </div>
+ <div id="right">
+    <div id="signin"><a href="signin.html"> Sign in</a></div>
+    <div id="get">Get started free</div>
+    <div id="nightmode"><span class="material-symbols-outlined">
+        bedtime
+        </span></div>
+ </div>`
+}
+
+let footer=()=>{
+    return `<div id="upper">
+    <div class="divs">
+        <h3>Product</h3>
+        <p>Product tour</p>
+        <p>Heatmaps</p>
+        <p>Session recordings</p>
+        <p>Feedback</p>
+        <p>Surveys</p>
+        <hr>
+        <p>Integrations</p>
+        <p>Survey templates</p>
+        <p>Dashboard</p>
+        <p>Workspacesp</p>
+        <p>Highlights</p>
+    </div>
+    <div class="divs">
+       <h3>Why Hotjar</h3> 
+       <p>For product teams</p>
+       <p>For product managers</p>
+       <p>For product designers</p>
+       <p>For researchers</p>
+       <p>Case studies</p>
+    </div>
+    <div class="divs">
+        <h3>Learn</h3>
+        <p>Guides</p>
+        <p>Blog</p>
+        <p>Product forge
+        </p>
+        <p>Product updates</p>
+        <p>Support</p>
+        <p>Product management 101</p>
+        <p>Product management glossary</p>
+        <p>UX design</p>
+        <p>Product experience insights</p>
+        <p>Website tracking</p>
+    </div>
+    <div class="divs">
+    <h3>Company</h3>
+    <p>About us</p>
+    <p>Careers</p>
+    <p>Privacy</p>
+    <p>Press</p>
+    </div>
+    <div class="divs">
+        <h3>Compliance</h3>
+        <p>Legal overview</p>
+        <p>Compliance & security</p>
+        <p>Terms of Service</p>
+        <p>Privacy Policy</p>
+        <p>Acceptable Use Policy</p>
+        
+        </div>  
+    </div>
+    <hr>
+    <br>
+    <div id="span">
+<span class="fa fa-fw fa-facebook"></span>
+<span class="fa fa-fw fa-twitter"></span>
+<span class="fa fa-fw fa-linkedin"></span>
+<span class="fa fa-fw fa-youtube"></span>
+    </div>
+    <div id="footerend">
+        <div>
+            <img src="https://www.hotjar.com/images/hotjar-logo-small.svg" alt="">
+            Copyright © 2014 - 2022 Hotjar Ltd. All rights reserved.</div>
+        <div>
+        <div>English (United States) |</div>
+         <div>Cookie info</div>
+        <div>Cookie settings
+        </div>
+        <div>Do not track
+        </div>
+        
+    </div>`
+}
+
+export {navbar,footer};
---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+import {navbar,footer} from "../components/navbar.js"
+
+
+ document.querySelector('#footer').innerHTML=footer();
+ document.querySelector('#navbar').innerHTML=navbar();
