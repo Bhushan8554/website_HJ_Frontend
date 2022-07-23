525323142001989363faa28158e9f3eca85e4b06
shitalkumar87 <101393465+shitalkumar87@users.noreply.github.com>
2022-07-23 11:49:12 +0530

    integration page added

--- scripts/product.js
+++ scripts/product.js
@@ -56,3 +56,18 @@ let changeContent=(event)=>{
     console.log(event)
     event.target.lastElementChild.style.display="block"
 };
+
+import {navbar,footer,productFun,whyhotrajFun,LearnFun,companyho} from "../components/navbar.js"
+ 
+
+document.querySelector('#navbar').innerHTML=navbar();
+document.querySelector('#learning').addEventListener("click",LearnFun);
+document.querySelector('#product').addEventListener("click",productFun);
+document.querySelector('#whyhotraj').addEventListener("click",whyhotrajFun);
+document.querySelector('#company').addEventListener("click",companyho);
+document.querySelector('#footer').innerHTML=footer();
+
+ 
+ 
+ 
+ 
